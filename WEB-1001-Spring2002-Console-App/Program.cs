89736e0fcdd6f858b4a6ffeb1a49a6ec91ce943d
Brent Ritchie <ritchie.brent@gmail.com>
2022-05-19 17:53:21 -0400

    Add project files.

---
+++ WEB-1001-Spring2002-Console-App/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace WEB_1001_Spring2002_Console_App
+{
+    internal class Program
+    {
+        public static readonly string x = "hello";
+
+        static void Main(string[] args)
+        {
+            int r = 5;
+            int g = 6;
+            int b = 7;
+
+            int c = r + g;
+
+            Console.WriteLine(c);
+
+            Hello();
+
+            Goodbye();
+        }
+
+        static void Hello()
+        { Console.WriteLine("Hello"); }
+
+        static void Goodbye()
+        {
+            Console.WriteLine(x);
+        }
+    }
+}
